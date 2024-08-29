db01aed6d6c68f02c7180fc84ef8cdd9eb604a79
Shs0160 <aladinsue@gmail.com>
2024-08-29 22:44:07 +0900

    24/08/29

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
         body: SingleChildScrollView(
